98713b53bc0cb120c5dd49c81dfd4fce6a4c73a0
Misbahu Sharfuddeen Zubair School of Physical Sciences MSZubair Computing <MSZubair@uclan.ac.uk>
2020-04-29 16:36:50 +0100

    section6

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ class Todo extends React.Component{
         super(props)
         this.state = {
             items: ['Buy milk', 'Take out trash', 'Renew Netflix subscription']
-        }        
+        }
+        this.createItem = this.createItem.bind(this)
+    }
+
+    createItem(text){
+        let temp = this.state.items
+        temp.push(text)
+        this.setState({items: temp})   
     }
 
     render(){
         return (
             <div className="Todo">
-                <Header/>
+                <Header create = {this.createItem}/>
                 {
                     this.state.items.map(function(data,index){
                         return <Item text={data} key={index}/>
@@ -21,13 +28,16 @@ class Todo extends React.Component{
 } 
 
 function Header(props){
+    let textInput = React.createRef();
+
     return(
-        <div className="Header">
+        <div className = "Header">
             <h1>Todo App</h1>
             <div>
-                <input type ="text"></input>
-                <button>+Item</button>
+                <input type ="text" ref={textInput}></input>
+                <button onClick={function(){if(textInput.current.value!="") props.create(textInput.current.value); textInput.current.value=""}}>+Item</button>
             </div>
+            
         </div>
     )
 }
